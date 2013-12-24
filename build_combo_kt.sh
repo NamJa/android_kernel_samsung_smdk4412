f5c2ec3df009591310d0172a4dc5c39113a9f6bd
ckh469 <ckh469@naver.com>
2013-12-24 23:51:12 +0900

    Update typo KT_model

--- build_combo_kt.sh
+++ build_combo_kt.sh
@@ -18,7 +18,7 @@ CUSTOM_PATH=note
 MODE=DUAL	
 fi
 
-displayversion=KT_Devil2-2.0.4
+displayversion=KT_Devil2-2.2.0
 version=$displayversion-$TARGET-$MODE-$(date +%Y%m%d)
 
 if [ -e boot.img ]; then
--- build_combo_skt.sh
+++ build_combo_skt.sh
@@ -18,7 +18,7 @@ CUSTOM_PATH=note
 MODE=DUAL	
 fi
 
-displayversion=SKT_Devil2-2.0.4
+displayversion=SKT_Devil2-2.2.0
 version=$displayversion-$TARGET-$MODE-$(date +%Y%m%d)
 
 if [ -e boot.img ]; then
